2a948b1b42f5f85ce674a84a026a5bdb150b6caf
Neil Spurgeon <neilspurgeon@gmail.com>
2015-06-01 10:04:56 -0700

    cleaned up

--- models/client.js
+++ models/client.js
@@ -122,10 +122,10 @@ Number.prototype.toHHMMSS = function () {
     var minutes = Math.floor((secNum - (hours * 3600)) / 60);
     var seconds = secNum - (hours * 3600) - (minutes * 60);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    var time    = hours+':'+minutes+':'+seconds;
+    if (hours   < 10) {hours   = "0" + hours;}
+    if (minutes < 10) {minutes = "0" + minutes;}
+    if (seconds < 10) {seconds = "0" + seconds;}
+    var time    = hours + ':' + minutes + ':' + seconds;
     return time;
 };
 
