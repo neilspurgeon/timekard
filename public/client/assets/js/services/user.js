3a0a2f6c2948259145f3a3f7d6a3ebf559626eee
Neil <neilspurgeon@gmail.com>
2015-07-31 15:03:33 -0700

    added service

---
+++ public/client/assets/js/services/user.js
@@ -0,0 +1,9 @@
+appServices.factory('UserService', function($http) {
+  return {
+    logIn: function(email, password) {
+        return $http.post('/login', {email: email, password: password});
+    },
+    logOut: function() {
+    }
+  };
+});
